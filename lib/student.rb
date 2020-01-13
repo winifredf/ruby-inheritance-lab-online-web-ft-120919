626cb1cfbf1b32180eb2b01f2deb1086c9ea8718
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 14:41:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,7 @@ class Student < User
     @last_name = last_name
   end
   
-  def learn(KNOWLEDGE)
+  def learn(knowledge)
     ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   end
   
