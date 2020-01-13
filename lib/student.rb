89a68d4320c2dc070f9b74c384179d68833ab80e
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 14:36:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,14 @@ require_relative "./user.rb"
 
 class Student < User
   
+  attr_accessor :first_name, :last_name
+  
+  def initialize(first_name,last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  def learn()
+  
 
 end
